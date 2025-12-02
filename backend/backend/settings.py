ff2b05a22047f10e05b95386a33b6d1f34684d73
Hanna Ruban <a.ruban126@gmail.com>
2025-12-02 17:22:32 +0300

    Test

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -4,7 +4,7 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'test-secret-key-change-me')
 
 DEBUG = False
 
