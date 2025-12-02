efdd3f79db0dd7ca986861edf5c5a73ed046ceeb
Hanna Ruban <a.ruban126@gmail.com>
2025-12-02 16:45:42 +0300

    Test

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -8,7 +8,7 @@ SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^
 
 DEBUG = False
 
-ALLOWED_HOSTS = ['localhost', '123.123.123.123', 'kittygramruban.publicvm.com']
+ALLOWED_HOSTS = ['localhost', '123.123.123.123', 'taskiruban.ddns.net']
 
 INSTALLED_APPS = [
     'api.apps.ApiConfig',
